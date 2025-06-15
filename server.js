6fbed403e4d9ab8d2793881494ed53a9e2c18558
mostafa <mostafaamin188027@gmail.com>
2025-06-15 23:18:19 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+import jsonServer from 'json-server';
+import auth from 'json-server-auth';
+import cors from 'cors';
+
+const app = jsonServer.create();
+const router = jsonServer.router('db.json');
+
+app.db = router.db;
+
+app.use(cors());
+app.use(jsonServer.bodyParser);
+app.use(auth);
+app.use(router);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`JSON Server running on http://localhost:${PORT}`);
+});
